2389084b642e727fb6251aea37e88bdfcad2fa11
Alexander <aleksandrchistov.ru@yandex.ru>
2021-10-24 18:22:03 +0700

    created function to replace children tags in pug templates

---
+++ src/core/utils.ts
@@ -0,0 +1,17 @@
+import {Props} from "./types";
+
+export const compileTemplate = (templatePugFn: (locals) => string, props: Props) => {
+    console.log(JSON.stringify(templatePugFn));
+    let template = templatePugFn(props);
+    console.log('compileToTemplate template', template);
+
+    const {children = {}} = props;
+
+    Object.keys(children).forEach(childTag => {
+        console.log('childTag', childTag);
+        template = template.replace(`<${childTag}></${childTag}>`, children[childTag].render());
+        console.log('replacedTemplate', template);
+    })
+
+    return template;
+};
