d3c5314f82d75110ff6d4d144d42ea8b98ab31b4
Alexander <aleksandrchistov.ru@yandex.ru>
2021-10-24 18:28:50 +0700

    rewrote list.ts file to match new dynamic approach

---
+++ src/components/link-list/link-list.ts
@@ -0,0 +1,49 @@
+import {Block} from "../../core/block";
+import {Props} from "../../core/types";
+import {compileTemplate} from '../../core/utils';
+import templatePug from "./link-list.pug";
+import './link-list.scss';
+
+const props: Props = {
+    items: [
+        {
+            href: '/src/screens/signin/signin.pug',
+            value: 'Sign in'
+        },
+        {
+            href: '/src/screens/signup/signup.pug',
+            value: 'Sign up'
+        },
+        {
+            href: '/src/screens/settings/settings.pug',
+            value: 'User settings'
+        },
+        {
+            href: '/src/screens/chat/chat.pug',
+            value: 'Chat (stub)'
+        },
+        {
+            href: '/src/screens/404/404.pug',
+            value: '404'
+        },
+        {
+            href: '/src/screens/500/500.pug',
+            value: '500'
+        }
+    ]
+}
+
+class LinkList extends Block {
+    constructor(propsObj: Props) {
+        // Создаём враппер DOM-элемент
+        super("div", propsObj);
+    }
+
+    render() {
+        const template = compileTemplate(templatePug, this.props);
+        console.log('List template', template);
+        return template;
+    }
+}
+
+export const itemListComponent = new LinkList(props);
