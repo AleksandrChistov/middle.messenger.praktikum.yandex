a327fe1f3a51f3c7d40ac89ae49843b8f1377b05
Alexander <aleksandrchistov.ru@yandex.ru>
2021-10-24 18:27:17 +0700

    rewrote main.ts file to build dynamic ui

---
+++ src/main.ts
@@ -0,0 +1,47 @@
+import {Block} from './core/block';
+import {Props} from "./core/types";
+import {compileTemplate} from './core/utils';
+import templatePug from './main.pug';
+import {itemListComponent} from './components/link-list/link-list';
+
+const props: Props = {
+    title: 'Chatly is saying hello!',
+    classTitle: 'title',
+    events: {
+        click: event => {
+            console.log(event);
+        },
+    },
+    children: {
+        ItemList: itemListComponent,
+    }
+}
+
+class Main extends Block {
+    constructor(propsObj: Props) {
+        // Создаём враппер DOM-элемент button
+        super("main", propsObj);
+    }
+
+    render() {
+        const template = compileTemplate(templatePug, this.props);
+        console.log('Main template', template);
+        return template;
+    }
+
+    componentDidMount() {
+        console.log('componentDidMount', this);
+        const root = document.getElementById('app');
+
+        root.appendChild(this.getContent());
+    }
+}
+
+const main = new Main(props);
+
+setTimeout(() => {
+    main.setProps({
+        title: 'New Title!',
+        classTitle: 'some other class'
+    })
+}, 4000);
