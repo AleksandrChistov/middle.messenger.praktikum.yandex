cfe9c966cb96940dd18ea324c68d0d437bb3ba2b
Alexander <aleksandrchistov.ru@yandex.ru>
2021-10-31 18:17:50 +0700

    made up the avatar component

---
+++ src/components/avatar/avatar.ts
@@ -0,0 +1,22 @@
+import {Block} from "../../core/block";
+import {Props} from "../../core/types";
+import {compileTemplateToElement} from '../../core/utils';
+import templatePug from "./avatar.pug";
+import './avatar.scss';
+
+interface AvatarProps extends Props {
+    avatarImgSrc?: string;
+    size?: string;
+}
+
+export class Avatar extends Block<AvatarProps> {
+    constructor(propsObj: AvatarProps) {
+        super("div", propsObj);
+    }
+
+    render() {
+        const element = compileTemplateToElement(templatePug, this.props);
+        console.log('Avatar template', element);
+        return element;
+    }
+}
