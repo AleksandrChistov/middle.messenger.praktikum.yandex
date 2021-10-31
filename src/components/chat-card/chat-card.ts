9313bf06386a146109e346aa823f21d67435f32e
Alexander <aleksandrchistov.ru@yandex.ru>
2021-10-31 18:46:43 +0700

    made up the chat-card component

---
+++ src/components/chat-card/chat-card.ts
@@ -0,0 +1,29 @@
+import {Block} from "../../core/block";
+import {Props} from "../../core/types";
+import {compileTemplateToElement} from '../../core/utils';
+import templatePug from "./chat-card.pug";
+import {Avatar} from "../avatar/avatar";
+import {Time} from "../time/time";
+import './chat-card.scss';
+
+interface ChatCardProps extends Props {
+    authorName: string;
+    textMessage?: string;
+    messageCount?: string;
+    children: {
+        Avatar: Avatar
+        Time: Time
+    }
+}
+
+export class ChatCard extends Block<ChatCardProps> {
+    constructor(propsObj: ChatCardProps) {
+        super("div", propsObj);
+    }
+
+    render() {
+        const element = compileTemplateToElement(templatePug, this.props);
+        console.log('ChatCard template', element);
+        return element;
+    }
+}
