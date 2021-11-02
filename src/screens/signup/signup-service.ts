a666b566fd88964f28f9f7d54fdf7c6b89b23722
Alexander <aleksandrchistov.ru@yandex.ru>
2021-11-02 12:36:19 +0700

    created the SignUpService for business logic purpose

---
+++ src/screens/signup/signup-service.ts
@@ -0,0 +1,163 @@
+import {TextInput} from "../../components/inputs/text/text-input";
+import {EmailInput} from "../../components/inputs/email/email-input";
+import {PhoneInput} from "../../components/inputs/phone/phone-input";
+import {PasswordInput} from "../../components/inputs/password/password-input";
+import {ErrorMessage} from "../../components/error-message/error-message";
+import {FormButton} from "../../components/form-button/form-button";
+import {HandleFormService} from "../../services/form-service";
+import {Props} from "../../core/types";
+
+export interface SignUpPageProps extends Props {}
+
+class SignUpService {
+    public handleFormService: HandleFormService; // TODO: use abstract class SOLID
+    public props: SignUpPageProps;
+
+    constructor() {
+        this.handleFormService = new HandleFormService(this.showError.bind(this));
+        this.props = getProps(this.handleFormService);
+    }
+
+    showError(errorMessage: string): void {
+        this.props.children.ErrorMessage.setProps({
+            textError: errorMessage,
+            addClass: errorMessage ? 'error-text--display' : '',
+        })
+    }
+}
+
+function getProps(handleFormService): SignUpPageProps {
+    return {
+        children: {
+            TextInput1: new TextInput({
+                label: 'Name',
+                id: 'first_name',
+                name: 'first_name',
+                inputClass: 'mb-5',
+                required: true,
+            }),
+            TextInput2: new TextInput({
+                label: 'Surname',
+                id: 'second_name',
+                name: 'second_name',
+                inputClass: 'mb-5',
+                required: true,
+            }),
+            TextInput3: new TextInput({
+                label: 'Login',
+                id: 'login',
+                name: 'login',
+                inputClass: 'mb-5',
+                required: true,
+            }),
+            EmailInput: new EmailInput({
+                label: 'Email',
+                id: 'email',
+                name: 'email',
+                inputClass: 'mb-5',
+                required: true,
+            }),
+            PhoneInput: new PhoneInput({
+                label: 'Phone',
+                id: 'phone',
+                name: 'phone',
+                inputClass: 'mb-5',
+                required: true,
+            }),
+            PasswordInput1: new PasswordInput({
+                label: 'Password',
+                id: 'password',
+                name: 'password',
+                inputClass: 'mb-5',
+                required: true,
+            }),
+            PasswordInput2: new PasswordInput({
+                label: 'Password (again)',
+                id: 'passwordAgain',
+                name: 'passwordAgain',
+                inputClass: 'mb-5',
+                required: true,
+            }),
+            ErrorMessage: new ErrorMessage({
+                addClass: 'form__error-text'
+            }),
+            FormButton: new FormButton({
+                type: 'submit',
+                text: 'Sign up',
+                addClass: 'mt-30'
+            })
+        },
+        events: {
+            focus: [
+                {
+                    id: "first_name",
+                    fn: event => handleFormService.handleFieldFocus(event)
+                },
+                {
+                    id: "second_name",
+                    fn: event => handleFormService.handleFieldFocus(event)
+                },
+                {
+                    id: "login",
+                    fn: event => handleFormService.handleFieldFocus(event)
+                },
+                {
+                    id: "email",
+                    fn: event => handleFormService.handleFieldFocus(event)
+                },
+                {
+                    id: "phone",
+                    fn: event => handleFormService.handleFieldFocus(event)
+                },
+                {
+                    id: "password",
+                    fn: event => handleFormService.handleFieldFocus(event)
+                },
+                {
+                    id: "passwordAgain",
+                    fn: event => handleFormService.handleFieldFocus(event)
+                }
+            ],
+            blur: [
+                {
+                    id: "first_name",
+                    fn: event => handleFormService.handleFieldBlur(event)
+                },
+                {
+                    id: "second_name",
+                    fn: event => handleFormService.handleFieldBlur(event)
+                },
+                {
+                    id: "login",
+                    fn: event => handleFormService.handleFieldBlur(event)
+                },
+                {
+                    id: "email",
+                    fn: event => handleFormService.handleFieldBlur(event)
+                },
+                {
+                    id: "phone",
+                    fn: event => handleFormService.handleFieldBlur(event)
+                },
+                {
+                    id: "password",
+                    fn: event => handleFormService.handleFieldBlur(event)
+                },
+                {
+                    id: "passwordAgain",
+                    fn: event => handleFormService.handleFieldBlur(event)
+                }
+            ],
+            submit: [
+                {
+                    id: "form",
+                    fn: event => handleFormService.handleFormSubmit(event)
+                }
+            ],
+        },
+    }
+}
+
+const signUpService = new SignUpService();
+
+export const props = signUpService.props;
--- src/screens/signup/signup.ts
+++ src/screens/signup/signup.ts
@@ -1,70 +1,7 @@
 import {Block} from '../../core/block';
-import {Props} from "../../core/types";
 import {compileTemplateToElement} from '../../core/utils';
 import templatePug from './signup.pug';
-import {TextInput} from '../../components/inputs/text/text-input';
-import {EmailInput} from "../../components/inputs/email/email-input";
-import {PhoneInput} from "../../components/inputs/phone/phone-input";
-import {PasswordInput} from "../../components/inputs/password/password-input";
-import {ErrorMessage} from "../../components/error-message/error-message";
-import {FormButton} from "../../components/form-button/form-button";
-
-interface SignUpPageProps extends Props {}
-
-const props: SignUpPageProps = {
-    children: {
-        TextInput1: new TextInput({
-            label: 'Name',
-            id: 'first_name',
-            name: 'first_name',
-            inputClass: 'mb-5',
-        }),
-        TextInput2: new TextInput({
-            label: 'Surname',
-            id: 'second_name',
-            name: 'second_name',
-            inputClass: 'mb-5',
-        }),
-        TextInput3: new TextInput({
-            label: 'Login',
-            id: 'login',
-            name: 'login',
-            inputClass: 'mb-5',
-        }),
-        EmailInput: new EmailInput({
-            label: 'Email',
-            id: 'email',
-            name: 'email',
-            inputClass: 'mb-5',
-        }),
-        PhoneInput: new PhoneInput({
-            label: 'Phone',
-            id: 'phone',
-            name: 'phone',
-            inputClass: 'mb-5',
-        }),
-        PasswordInput1: new PasswordInput({
-            label: 'Password',
-            id: 'password',
-            name: 'password',
-            inputClass: 'mb-5',
-        }),
-        PasswordInput2: new PasswordInput({
-            label: 'Password (again)',
-            id: 'newPassword',
-            name: 'newPassword',
-            inputClass: 'mb-5',
-        }),
-        ErrorMessage: new ErrorMessage({
-            addClass: 'form__error-text'
-        }),
-        FormButton: new FormButton({
-            type: 'submit',
-            text: 'Sign up',
-            addClass: 'mt-30'
-        })
-    }
-}
+import {SignUpPageProps, props} from "./signup-service";
 
 class SignUpPage extends Block<SignUpPageProps> {
     constructor(propsObj: SignUpPageProps) {
@@ -85,10 +22,4 @@ class SignUpPage extends Block<SignUpPageProps> {
     }
 }
 
-const signUpPage = new SignUpPage(props);
-
-setTimeout(() => {
-    signUpPage.props.children.TextInput1.setProps({
-        label: 'Test'
-    })
-}, 4000);
+new SignUpPage(props);
