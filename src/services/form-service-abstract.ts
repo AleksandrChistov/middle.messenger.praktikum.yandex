4414a7d7a09e33879e6645ab0cd4135e4a3f4975
Alexander <aleksandrchistov.ru@yandex.ru>
2021-11-02 13:05:36 +0700

    created the FormServiceAbstract class to highlight the common part

---
+++ src/services/form-service-abstract.ts
@@ -0,0 +1,11 @@
+import {HandleFormService} from "./form-service";
+
+export abstract class FormServiceAbstract {
+    public handleFormService: HandleFormService; // TODO: use abstract class SOLID
+
+    protected constructor() {
+        this.handleFormService = new HandleFormService(this.showError.bind(this));
+    }
+
+    protected abstract showError(errorMessage: string): void;
+}
