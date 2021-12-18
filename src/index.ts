65ba2098e13afa42745776261591a8b26fd8c4d0
Alexander <aleksandrchistov.ru@yandex.ru>
2021-12-18 18:06:04 +0700

    registered routes for 404 and 500 pages in the router component

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,8 @@ import {SignInPage} from "./screens/signin/signin";
 import {SignUpPage} from "./screens/signup/signup";
 import {SettingsPage} from "./screens/settings/settings";
 import {ChatPage} from "./screens/chat/chat";
+import {Page404} from "./screens/404/404";
+import {Page500} from "./screens/500/500";
 
 export const router = new Router("app");
 
@@ -11,4 +13,7 @@ router
   .use("/sign-up", SignUpPage)
   .use("/settings", SettingsPage)
   .use("/messenger", ChatPage)
+  .use("/404", Page404)
+  .use("/500", Page500)
   .start();
+
