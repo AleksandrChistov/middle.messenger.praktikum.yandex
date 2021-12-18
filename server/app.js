7037f269bc7ac5e7ca286086c7885f8e476895f1
Alexander <aleksandrchistov.ru@yandex.ru>
2021-12-18 14:00:32 +0700

    changed the express server to work with routing

--- server/app.js
+++ server/app.js
@@ -1,11 +1,13 @@
 const express = require('express');
-const path = require("path");
 
 const app = express();
-const PORT = 3000;
-const DIST_PATH = path.join(__dirname, '/../dist');
+const PORT = process.env.PORT || 3000;
 
-app.use(express.static(DIST_PATH));
+app.use(express.static(__dirname + '/../dist'));
+
+app.get('/*', (req, res) => {
+  res.sendFile(__dirname + '/../dist/index.html');
+});
 
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}...`);
