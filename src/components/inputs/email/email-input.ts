86b05a90837536c9c3e4c7ae76e210c269d7d0c3
Alexander <aleksandrchistov.ru@yandex.ru>
2022-01-02 20:13:37 +0700

    added optional value key to EmailInputProps

--- src/components/inputs/email/email-input.ts
+++ src/components/inputs/email/email-input.ts
@@ -1,12 +1,14 @@
 import {Block} from '../../../core/block';
 import {Props} from '../../../core/types';
-import {compileTemplateToElement} from '../../../core/utils';
+import {compileTemplateToElement} from '../../../core/utils/compile-template';
 import templatePug from './email-input.pug';
 import './email-input.scss';
+import {FieldName} from '../../../services/form-services/form-validation-service';
 
-interface EmailInputProps extends Props {
+export interface EmailInputProps extends Props {
 	id: string;
-	name: string;
+	name: FieldName;
+	value?: string;
 	label?: string;
 	labelClass?: string;
 	inputClass?: string;
