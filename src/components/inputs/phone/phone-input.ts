c08061fcd878889c315f05163a19d395a1a4700e
Alexander <aleksandrchistov.ru@yandex.ru>
2022-01-02 20:11:42 +0700

    added optional value key to PhoneInputProps

--- src/components/inputs/phone/phone-input.ts
+++ src/components/inputs/phone/phone-input.ts
@@ -1,12 +1,14 @@
 import {Block} from '../../../core/block';
 import {Props} from '../../../core/types';
-import {compileTemplateToElement} from '../../../core/utils';
+import {compileTemplateToElement} from '../../../core/utils/compile-template';
 import templatePug from './phone-input.pug';
 import './phone-input.scss';
+import {FieldName} from '../../../services/form-services/form-validation-service';
 
-interface PhoneInputProps extends Props {
+export interface PhoneInputProps extends Props {
 	id: string;
-	name: string;
+	name: FieldName;
+	value?: string;
 	label?: string;
 	labelClass?: string;
 	inputClass?: string;
