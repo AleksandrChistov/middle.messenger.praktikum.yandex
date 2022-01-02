798c8b2be45f2fe3a13a1489968cb418ddad6f45
Alexander <aleksandrchistov.ru@yandex.ru>
2022-01-02 20:15:37 +0700

    added null value for avatarImgSrc key of AvatarProps

--- src/components/avatar/avatar.ts
+++ src/components/avatar/avatar.ts
@@ -1,11 +1,11 @@
 import {Block} from '../../core/block';
 import {Props} from '../../core/types';
-import {compileTemplateToElement} from '../../core/utils';
+import {compileTemplateToElement} from '../../core/utils/compile-template';
 import templatePug from './avatar.pug';
 import './avatar.scss';
 
-interface AvatarProps extends Props {
-	avatarImgSrc?: string;
+export interface AvatarProps extends Props {
+	avatarImgSrc: string | null;
 	size?: string;
 }
 
