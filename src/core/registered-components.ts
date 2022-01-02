e37083e9da2d2a896fc67041c3f68a1fba92818e
Alexander <aleksandrchistov.ru@yandex.ru>
2022-01-02 19:20:57 +0700

    added registeredComponent constant

---
+++ src/core/registered-components.ts
@@ -0,0 +1,25 @@
+import {ChatCard} from "../components/chat-card/chat-card";
+import {SearchInput} from "../components/inputs/search/search-input";
+import {Avatar} from "../components/avatar/avatar";
+import {Time} from "../components/time/time";
+import {Message} from "../components/message/message";
+import {ErrorMessage} from "../components/error-message/error-message";
+import {TextInput} from "../components/inputs/text/text-input";
+import {PasswordInput} from "../components/inputs/password/password-input";
+import {FormButton} from "../components/form-button/form-button";
+import {EmailInput} from "../components/inputs/email/email-input";
+import {PhoneInput} from "../components/inputs/phone/phone-input";
+
+export const REGISTERED_COMPONENTS = {
+  TextInputComponent: TextInput,
+  EmailInputComponent: EmailInput,
+  PasswordInputComponent: PasswordInput,
+  PhoneInputComponent: PhoneInput,
+  SearchInputComponent: SearchInput,
+  FormButtonComponent: FormButton,
+  ErrorMessageComponent: ErrorMessage,
+  ChatCardComponent: ChatCard,
+  AvatarComponent: Avatar,
+  TimeComponent: Time,
+  MessageComponent: Message,
+}
