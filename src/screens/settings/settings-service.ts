f0c80480a5489302d03846d1d7d57991eb256f88
Alexander <aleksandrchistov.ru@yandex.ru>
2022-01-02 20:03:50 +0700

    created show and hide error method in settings-service.ts

--- src/screens/settings/settings-service.ts
+++ src/screens/settings/settings-service.ts
@@ -1,266 +1,268 @@
-import {Children, Props} from '../../core/types';
-import {HandleFormService} from '../../services/form-services/form-service';
+import {Events} from '../../core/types';
+import {Invalid} from '../../services/form-services/form-service';
 import {FieldName} from "../../services/form-services/form-validation-service";
 import {ShowErrorService} from "../../services/show-error-service";
-import {getErrorMessageFieldName} from "../../utils";
-import {TextInput} from '../../components/inputs/text/text-input';
-import {PasswordInput} from '../../components/inputs/password/password-input';
-import {FormButton} from '../../components/form-button/form-button';
-import {EmailInput} from '../../components/inputs/email/email-input';
-import {PhoneInput} from '../../components/inputs/phone/phone-input';
-import {ErrorMessage} from '../../components/error-message/error-message';
+import {ERROR_ACTIVE_CLASS} from '../../components/error-message/error-message';
 import {router} from "../../index";
 import {UserLogOutController} from "../../controllers/auth-controllers/logout-controller";
+import {FIELD_ERROR_TEXT} from "../../services/form-services/constants";
+import store from "../../store/store";
+import {ChangeUserProfileController} from "../../controllers/user-profile-controller/change-user-profile-controller";
 
-export interface SettingsPageProps extends Props {
-  children: Children;
-}
 
 class SettingsService extends ShowErrorService {
-	public props: SettingsPageProps;
+  private _showError(path: string, eventName: string, error: Invalid, fieldName: FieldName): void {
+    const LoginErrorText = FIELD_ERROR_TEXT[fieldName];
+    const textError = error?.text ? LoginErrorText.text : LoginErrorText.length;
 
-	constructor() {
-    super();
-    this.props = getProps(this.handleFormService);
-	}
-}
+    const errorProps = {
+      addClass: ERROR_ACTIVE_CLASS,
+      textError: textError
+    }
 
-function getProps(handleFormService: HandleFormService): SettingsPageProps {
-	return {
-		children: {
-			textInputComponent1: new TextInput({
-				label: 'Name',
-				id: 'first_name',
-				name: FieldName.FirstName,
-				inputClass: 'mb-5',
-				required: true,
-			}),
-      [getErrorMessageFieldName(FieldName.FirstName)]: new ErrorMessage({
-        addClass: 'form__error-text',
-      }),
-			textInputComponent2: new TextInput({
-				label: 'Surname',
-				id: 'second_name',
-				name: FieldName.SecondName,
-				inputClass: 'mb-5',
-				required: true,
-			}),
-      [getErrorMessageFieldName(FieldName.SecondName)]: new ErrorMessage({
-        addClass: 'form__error-text',
-      }),
-			textInputComponent3: new TextInput({
-				label: 'Display name',
-				id: 'display_name',
-				name: 'display_name',
-				inputClass: 'mb-5',
-				required: true,
-			}),
-			textInputComponent4: new TextInput({
-				label: 'Login',
-				id: 'login',
-				name: FieldName.Login,
-				inputClass: 'mb-5',
-				required: true,
-			}),
-      [getErrorMessageFieldName(FieldName.Login)]: new ErrorMessage({
-        addClass: 'form__error-text',
-      }),
-			emailInputComponent: new EmailInput({
-				label: 'Email',
-				id: 'email',
-				name: FieldName.Email,
-				inputClass: 'mb-5',
-				required: true,
-			}),
-      [getErrorMessageFieldName(FieldName.Email)]: new ErrorMessage({
-        addClass: 'form__error-text',
-      }),
-			phoneInputComponent: new PhoneInput({
-				label: 'Phone',
-				id: 'phone',
-				name: FieldName.Phone,
-				inputClass: 'mb-5',
-				required: true,
-			}),
-      [getErrorMessageFieldName(FieldName.Phone)]: new ErrorMessage({
-        addClass: 'form__error-text',
-      }),
-			formButtonComponent1: new FormButton({
-				type: 'submit',
-				text: 'Change data',
-				addClass: 'mt-20',
-			}),
-			passwordInputComponent1: new PasswordInput({
-				label: 'Old password',
-				id: 'oldPassword',
-				name: FieldName.OldPassword,
-				inputContainerClass: 'mb-5',
-				required: true,
-			}),
-      [getErrorMessageFieldName(FieldName.OldPassword)]: new ErrorMessage({
-        addClass: 'form__error-text',
-      }),
-			passwordInputComponent2: new PasswordInput({
-				label: 'Password',
-				id: 'password',
-				name: FieldName.Password,
-				inputContainerClass: 'mb-5',
-				required: true,
-			}),
-      [getErrorMessageFieldName(FieldName.Password)]: new ErrorMessage({
-        addClass: 'form__error-text',
-      }),
-			passwordInputComponent3: new PasswordInput({
-				label: 'Password (again)',
-				id: 'newPassword',
-				name: FieldName.NewPassword,
-				inputContainerClass: 'mb-5',
-				required: true,
-			}),
-      [getErrorMessageFieldName(FieldName.NewPassword)]: new ErrorMessage({
-        addClass: 'form__error-text',
-      }),
-			formButtonComponent2: new FormButton({
-				type: 'submit',
-				text: 'Change password',
-				addClass: 'mt-20',
-			}),
-		},
-		events: {
-      click: [
-        {
-          id: 'goToChat',
-          fn: event => {
-            event.preventDefault();
-            router.go('/messenger');
-          },
+    store.set(path, errorProps, eventName);
+  }
+
+  private _hideError(path: string, eventName: string): void {
+    const errorProps = {
+      addClass: '',
+      textError: ''
+    }
+    store.set(path, errorProps, eventName);
+  }
+
+  public settingsEvents: Events = {
+    click: [
+      {
+        id: 'goToChat',
+        fn: event => {
+          event.preventDefault();
+          router.go('/messenger');
         },
-        {
-          id: 'logout',
-          fn: event => {
-            event.preventDefault();
-            UserLogOutController.logOut();
-          },
+      },
+      {
+        id: 'logout',
+        fn: event => {
+          event.preventDefault();
+          UserLogOutController.logOut();
         },
-      ],
-			focus: [
-				{
-					id: 'first_name',
-					fn: event => {
-						handleFormService.handleFieldFocus(event);
-					},
-				},
-				{
-					id: 'second_name',
-					fn: event => {
-						handleFormService.handleFieldFocus(event);
-					},
-				},
-				{
-					id: 'login',
-					fn: event => {
-						handleFormService.handleFieldFocus(event);
-					},
-				},
-				{
-					id: 'email',
-					fn: event => {
-						handleFormService.handleFieldFocus(event);
-					},
-				},
-				{
-					id: 'phone',
-					fn: event => {
-						handleFormService.handleFieldFocus(event);
-					},
-				},
-				{
-					id: 'oldPassword',
-					fn: event => {
-						handleFormService.handleFieldFocus(event);
-					},
-				},
-				{
-					id: 'password',
-					fn: event => {
-						handleFormService.handleFieldFocus(event);
-					},
-				},
-				{
-					id: 'newPassword',
-					fn: event => {
-						handleFormService.handleFieldFocus(event);
-					},
-				},
-			],
-			blur: [
-				{
-					id: 'first_name',
-					fn: event => {
-						handleFormService.handleFieldBlur(event);
-					},
-				},
-				{
-					id: 'second_name',
-					fn: event => {
-						handleFormService.handleFieldBlur(event);
-					},
-				},
-				{
-					id: 'login',
-					fn: event => {
-						handleFormService.handleFieldBlur(event);
-					},
-				},
-				{
-					id: 'email',
-					fn: event => {
-						handleFormService.handleFieldBlur(event);
-					},
-				},
-				{
-					id: 'phone',
-					fn: event => {
-						handleFormService.handleFieldBlur(event);
-					},
-				},
-				{
-					id: 'oldPassword',
-					fn: event => {
-						handleFormService.handleFieldBlur(event);
-					},
-				},
-				{
-					id: 'password',
-					fn: event => {
-						handleFormService.handleFieldBlur(event);
-					},
-				},
-				{
-					id: 'newPassword',
-					fn: event => {
-						handleFormService.handleFieldBlur(event);
-					},
-				},
-			],
-			submit: [
-				{
-					id: 'form-profile',
-					fn: event => {
-						handleFormService.handleFormSubmit(event);
-					},
-				},
-				{
-					id: 'form-password',
-					fn: event => {
-						handleFormService.handleFormSubmit(event);
-					},
-				},
-			],
-		},
-	};
+      },
+    ],
+    focus: [
+      {
+        id: 'first_name',
+        fn: event => {
+          this.handleFormService.handleFieldFocus(event);
+        },
+      },
+      {
+        id: 'second_name',
+        fn: event => {
+          this.handleFormService.handleFieldFocus(event);
+        },
+      },
+      {
+        id: 'login',
+        fn: event => {
+          this.handleFormService.handleFieldFocus(event);
+        },
+      },
+      {
+        id: 'email',
+        fn: event => {
+          this.handleFormService.handleFieldFocus(event);
+        },
+      },
+      {
+        id: 'phone',
+        fn: event => {
+          this.handleFormService.handleFieldFocus(event);
+        },
+      },
+      {
+        id: 'oldPassword',
+        fn: event => {
+          this.handleFormService.handleFieldFocus(event);
+        },
+      },
+      {
+        id: 'password',
+        fn: event => {
+          this.handleFormService.handleFieldFocus(event);
+        },
+      },
+      {
+        id: 'newPassword',
+        fn: event => {
+          this.handleFormService.handleFieldFocus(event);
+        },
+      },
+    ],
+    blur: [
+      {
+        id: 'first_name',
+        fn: event => {
+          const error = this.handleFormService.handleFieldBlur(event);
+
+          if (!error) {
+            this._hideError('settingsPage.errorName', 'errorName');
+          } else {
+            this._showError('settingsPage.errorName', 'errorName', error, FieldName.FirstName);
+          }
+        },
+      },
+      {
+        id: 'second_name',
+        fn: event => {
+          const error = this.handleFormService.handleFieldBlur(event);
+
+          if (!error) {
+            this._hideError('settingsPage.errorSurname', 'errorSurname');
+          } else {
+            this._showError('settingsPage.errorSurname', 'errorSurname', error, FieldName.SecondName);
+          }
+        },
+      },
+      {
+        id: 'login',
+        fn: event => {
+          const error = this.handleFormService.handleFieldBlur(event);
+
+          if (!error) {
+            this._hideError('settingsPage.errorLogin', 'errorLogin');
+          } else {
+            this._showError('settingsPage.errorLogin', 'errorLogin', error, FieldName.Login);
+          }
+        },
+      },
+      {
+        id: 'email',
+        fn: event => {
+          const error = this.handleFormService.handleFieldBlur(event);
+
+          if (!error) {
+            this._hideError('settingsPage.errorEmail', 'errorEmail');
+          } else {
+            this._showError('settingsPage.errorEmail', 'errorEmail', error, FieldName.Email);
+          }
+        },
+      },
+      {
+        id: 'phone',
+        fn: event => {
+          const error = this.handleFormService.handleFieldBlur(event);
+
+          if (!error) {
+            this._hideError('settingsPage.errorPhone', 'errorPhone');
+          } else {
+            this._showError('settingsPage.errorPhone', 'errorPhone', error, FieldName.Phone);
+          }
+        },
+      },
+      {
+        id: 'oldPassword',
+        fn: event => {
+          const error = this.handleFormService.handleFieldBlur(event);
+
+          if (!error) {
+            this._hideError('settingsPage.errorOldPassword', 'errorOldPassword');
+          } else {
+            this._showError('settingsPage.errorOldPassword', 'errorOldPassword', error, FieldName.OldPassword);
+          }
+        },
+      },
+      {
+        id: 'password',
+        fn: event => {
+          const error = this.handleFormService.handleFieldBlur(event);
+
+          if (!error) {
+            this._hideError('settingsPage.errorPassword', 'errorPassword');
+          } else {
+            this._showError('settingsPage.errorPassword', 'errorPassword', error, FieldName.Password);
+          }
+        },
+      },
+      {
+        id: 'newPassword',
+        fn: event => {
+          const error = this.handleFormService.handleFieldBlur(event);
+
+          if (!error) {
+            this._hideError('settingsPage.errorPasswordAgain', 'errorPasswordAgain');
+          } else {
+            this._showError('settingsPage.errorPasswordAgain', 'errorPasswordAgain', error, FieldName.PasswordAgain);
+          }
+        },
+      },
+    ],
+    submit: [
+      {
+        id: 'form-profile',
+        fn: event => {
+          event.preventDefault();
+          const formValidElements = this.handleFormService.validateForm(event);
+
+          const isFormValid = formValidElements.every(element => {
+            if (!element) return true;
+
+            if (!element.invalid && element.dataName) {
+              this._hideError(`settingsPage.${element.dataName}`, element.dataName);
+              return true;
+            } else {
+              this._showError(`settingsPage.${element.dataName}`, element.dataName, element.invalid, element.fieldName);
+              return false;
+            }
+          })
+
+          if (!isFormValid) {
+            return;
+          }
+
+          const formData = this.handleFormService.handleFormSubmit(event);
+
+          if (!formData) {
+            return;
+          }
+
+          ChangeUserProfileController.changeData(formData);
+        },
+      },
+      {
+        id: 'form-password',
+        fn: event => {
+          event.preventDefault();
+          const formValidElements = this.handleFormService.validateForm(event);
+
+          const isFormValid = formValidElements.every(element => {
+            if (!element) return true;
+
+            if (!element.invalid && element.dataName) {
+              this._hideError(`settingsPage.${element.dataName}`, element.dataName);
+              return true;
+            } else {
+              this._showError(`settingsPage.${element.dataName}`, element.dataName, element.invalid, element.fieldName);
+              return false;
+            }
+          })
+
+          if (!isFormValid) {
+            return;
+          }
+
+          const formData = this.handleFormService.handleFormSubmit(event);
+
+          if (!formData) {
+            return;
+          }
+
+          // запрашиваем данные у контроллера
+        },
+      },
+    ],
+  }
 }
 
 const settingsService = new SettingsService();
 
-export const {props} = settingsService;
+export const {settingsEvents} = settingsService;
--- src/screens/settings/settings.ts
+++ src/screens/settings/settings.ts
@@ -1,12 +1,18 @@
 import {Block} from '../../core/block';
-import {compileTemplateToElement} from '../../core/utils';
+import {compileTemplateToElement} from "../../core/utils/compile-template";
 import templatePug from './settings.pug';
 import './settings.scss';
-import {props, SettingsPageProps} from './settings-service';
+import {SettingsPageProps} from "./types";
+import {SETTINGS_INITIAL_STATE} from "../../store/initialState/settings-initial-state";
+import {Events} from "../../core/types";
+import {settingsEvents} from "./settings-service";
+
+
+export const UPDATE_USER_PROFILE = 'Update User Profile';
 
 export class SettingsPage extends Block<SettingsPageProps> {
-	constructor(propsObj: SettingsPageProps = props, rootId) {
-		super('main', 'settings-page-block', propsObj, rootId);
+	constructor(propsObj: SettingsPageProps = SETTINGS_INITIAL_STATE, events: Events = settingsEvents, rootId?: string) {
+		super('main', 'settings-page-block', propsObj, events, rootId);
 	}
 
 	render() {
