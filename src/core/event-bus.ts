c8668dc9f8a8e3be709f7dc6159d5b23cdd117dd
Alexander <aleksandrchistov.ru@yandex.ru>
2022-01-03 21:17:30 +0700

    added subscriber to EventBus methods

--- src/core/event-bus.ts
+++ src/core/event-bus.ts
@@ -1,26 +1,37 @@
 type AnyFunc = (...args: any[]) => any | void;
 
 export class EventBus {
-	private listeners: Record<string, AnyFunc[]>;
+	private listeners: Record<string, {subscriber: unknown, callback: AnyFunc}[]>;
 
 	constructor() {
 		this.listeners = {};
 	}
 
-	on(event: string, callback: AnyFunc) {
+	on(event: string, callback: AnyFunc, subscriber: unknown) {
 		if (!this.listeners[event]) {
 			this.listeners[event] = [];
 		}
 
-		this.listeners[event].push(callback);
+    const isAlreadyRegistered = this.listeners[event]
+      .some(listener => listener.subscriber === subscriber && listener.callback === callback);
+
+    if (isAlreadyRegistered) {
+      return;
+    }
+
+		this.listeners[event].push({
+      subscriber,
+      callback
+    });
 	}
 
-	off(event: string, callback: AnyFunc) {
+	off(event: string, callback: AnyFunc, subscriber: unknown) {
 		if (!this.listeners[event]) {
 			throw new Error(`There is no event: ${event}`);
 		}
 
-		this.listeners[event] = this.listeners[event].filter(fn => fn !== callback);
+		this.listeners[event] = this.listeners[event]
+      .filter(listener => listener.subscriber !== subscriber || listener.callback !== callback);
 	}
 
 	emit(event: string, ...args: any[]) {
@@ -28,8 +39,8 @@ export class EventBus {
 			throw new Error(`There is no event: ${event}`);
 		}
 
-		this.listeners[event].forEach(fn => {
-			fn(...args);
+		this.listeners[event].forEach(listener => {
+      listener.callback.call(listener.subscriber, ...args);
 		});
 	}
 }
