57fb6c00cf09fd4e19316d3a0bc51108d7feb7f1
Alexander <aleksandrchistov.ru@yandex.ru>
2022-01-06 19:40:05 +0700

    created CreateChatController

---
+++ src/controllers/chat-controllers/create-chat-controller.ts
@@ -0,0 +1,54 @@
+import {Options, ResponseType} from "../../services/http-service";
+import {ErrorResponse} from "../../api/types";
+import {CreateChatAPI, CreateChatResponse} from "../../api/chat-api/create-chat-api";
+import {Indexed} from "../../core/types";
+import {isArray} from "../../utils";
+import {GetChatsController} from "./get-chats-controller";
+
+type CreateChatFormModel = {
+  title: string;
+}
+
+const createChatAPI = new CreateChatAPI();
+
+export class CreateChatController {
+  static async create(data: CreateChatFormModel): Promise<void> {
+    try {
+      // Запускаем крутилку
+      createChatAPI.create(prepareDataToRequest(data))
+        .then((response: CreateChatResponse | ErrorResponse | null) => {
+          if (isErrorResponse(response)) {
+            throw new Error(response.reason);
+          }
+
+          GetChatsController.get();
+        })
+        .catch((error) => {
+          console.error(error, data);
+          // Останавливаем крутилку
+        })
+    } catch (error) {
+      console.error(error, data);
+      // Логика обработки ошибок
+    }
+  }
+}
+
+function isErrorResponse(response: Indexed | Indexed[] | null): response is ErrorResponse {
+  return response !== null && isNotArray(response) && !!response.reason;
+}
+
+function isNotArray(response: Indexed | Indexed[]): response is Indexed {
+  return !isArray(response);
+}
+
+function prepareDataToRequest(data: CreateChatFormModel): Options {
+  return {
+    withCredentials: true,
+    responseType: ResponseType.json,
+    headers: {
+      'content-type': 'application/json',
+    },
+    data: data,
+  }
+}
