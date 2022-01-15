02bf756d3c557f71f661da3179b8de791d255145
Alexander <aleksandrchistov.ru@yandex.ru>
2022-01-15 22:54:21 +0700

    fixed Error: Routing problem

--- server/app.js
+++ server/app.js
@@ -1,12 +1,13 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static(__dirname + '/../dist'));
+app.use(express.static(path.join(__dirname, '../dist')));
 
 app.get('/*', (req, res) => {
-  res.sendFile(__dirname + '/../dist/index.html');
+  res.sendFile('index.html', { root: path.join(__dirname, '../dist') });
 });
 
 app.listen(PORT, () => {
